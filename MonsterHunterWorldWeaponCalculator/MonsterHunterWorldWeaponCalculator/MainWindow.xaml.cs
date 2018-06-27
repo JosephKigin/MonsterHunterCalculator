1578dd5c51c7600fc846875a7eabe94eff0a8b0f
JosephKigin <38922279+JosephKigin@users.noreply.github.com>
2018-06-27 11:37:43 -0500

    Add files via upload

---
+++ MonsterHunterWorldWeaponCalculator/MonsterHunterWorldWeaponCalculator/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MonsterHunterWorldWeaponCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public float SetDefaultRawSharpness(string sharpnessColor) //Takes the name of a color and gives back a value for sharpness
+        {
+            float sharpnessValue;
+
+            switch (sharpnessColor)
+            {
+                case "Red":
+                    sharpnessValue = .5f;
+                    break;
+                case "Orange":
+                    sharpnessValue = .75f;
+                    break;
+
+                case "Yellow":
+                    sharpnessValue = 1f;
+                    break;
+                case "Green":
+                    sharpnessValue = 1.05f;
+                    break;
+                case "Blue":
+                    sharpnessValue = 1.2f;
+                    break;
+                case "White":
+                    sharpnessValue = 1.32f;
+                    break;
+                default:
+                    sharpnessValue = 1;
+                    break;
+            }
+
+            return sharpnessValue;
+        }
+    }
+}
---
+++ MonsterHunterWorldWeaponCalculator/MonsterHunterWorldWeaponCalculator/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterHunterWorldWeaponCalculator
+{
+    class Weapon
+    {
+        public float Class { get; set; }
+        public float DefaultRawDamage { get; set; }
+        public float DefaultElementalDamage { get; set; }
+        public float DefaultAffinity { get; set; }
+        public float DefaultRawSharpness { get; set; }      //.5=red; .75=orange; 1=yellow; 1.05=green; 1.2=blue; 1.32=white
+        public int DefualtElementalSharpness { get; set; } //.25=red; .5=orange; .75=yellow; 1=green; 1.0625=blue; 1.125=white
+
+        public Weapon()
+        {
+
+        }
+
+        public float CalculateDamage(float attackBuff, float affinityBuff)
+        {
+            float calculatedDamage = (DefaultRawDamage * DefaultRawSharpness /* * motion * monsterArmor * quest/rage? */ );
+        }
+    }
+}
