4e73f378b8311229467495d06085ae01182b40b6
JosephKigin <38922279+JosephKigin@users.noreply.github.com>
2018-07-03 16:10:36 -0500

    Whole Project
    Added functionality to text boxes.
    Added logic for form to set sharpness and weapon modifiers.
    Made the main form create a weapon and input all data needed.

--- MonsterHunterWorldWeaponCalculator/MonsterHunterWorldWeaponCalculator/MainWindow.xaml.cs
+++ MonsterHunterWorldWeaponCalculator/MonsterHunterWorldWeaponCalculator/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.Xaml;
 
 namespace MonsterHunterWorldWeaponCalculator
 {
@@ -20,41 +22,120 @@ namespace MonsterHunterWorldWeaponCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
+
+       /* public TextBox RawDamageTextBox
+        {
+            get { return textBoxRawDamage; }
+            set { textBoxRawDamage = value; }
+        }*/
+
         public MainWindow()
         {
             InitializeComponent();
         }
-        public float SetDefaultRawSharpness(string sharpnessColor) //Takes the name of a color and gives back a value for sharpness
+
+        //Takes the name of a sharpness color and gives back a value for sharpness. sharpnessValues[0] = raw sharpnessValues[1] = elemental
+        public float[] SetSharpness(string sharpnessColor)
         {
-            float sharpnessValue;
+            float[] sharpnessValues = new float[2];
 
             switch (sharpnessColor)
             {
                 case "Red":
-                    sharpnessValue = .5f;
+                    sharpnessValues[0] = .5f;
+                    sharpnessValues[1] = .25f;
                     break;
                 case "Orange":
-                    sharpnessValue = .75f;
+                    sharpnessValues[0] = .75f;
+                    sharpnessValues[1] = .5f;
                     break;
 
                 case "Yellow":
-                    sharpnessValue = 1f;
+                    sharpnessValues[0] = 1f;
+                    sharpnessValues[1] = .75f;
                     break;
                 case "Green":
-                    sharpnessValue = 1.05f;
+                    sharpnessValues[0] = 1.05f;
+                    sharpnessValues[1] = 1f;
                     break;
                 case "Blue":
-                    sharpnessValue = 1.2f;
+                    sharpnessValues[0] = 1.2f;
+                    sharpnessValues[1] = 1.0625f;
                     break;
                 case "White":
-                    sharpnessValue = 1.32f;
+                    sharpnessValues[0] = 1.32f;
+                    sharpnessValues[1] = 1.125f;
                     break;
                 default:
-                    sharpnessValue = 1;
+                    sharpnessValues[0] = 1;
+                    sharpnessValues[1] = 1;
                     break;
             }
 
-            return sharpnessValue;
+            return sharpnessValues;
+        }
+        //Takes the name of a weapon class and gives a modifier value.
+        public float SetClassModifier(string weaponClass)
+        {
+            float classModifier;
+            switch(weaponClass)
+            {
+                case "Hammer":
+                    classModifier = 5.2f;
+                    break;
+
+                case "Hunting Horn":
+                    classModifier = 4.2f;
+                    break;
+
+                case "Switch Axe":
+                    classModifier = 3.5f;
+                    break;
+
+                case "Great Sword":
+                    classModifier = 4.8f;
+                    break;
+
+                case "Charge Blade":
+                    classModifier = 3.6f;
+                    break;
+                case "Long Sword":
+                    classModifier = 3.3f;
+                    break;
+                case "Insect Glaive":
+                    classModifier = 3.1f;
+                    break;
+
+                case "Lance":
+                    classModifier = 2.3f;
+                    break;
+
+                case "Gunlance":
+                    classModifier = 2.3f;
+                    break;
+
+                case "Sword and Shield":
+                    classModifier = 1.4f;
+                    break;
+
+                case "Duel Blades":
+                    classModifier = 1.4f;
+                    break;
+                default:
+                    classModifier = 1f;
+                    break;
+            }
+
+            return classModifier;
+        }
+
+        private void buttonCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            float[] sharpnessValues = SetSharpness(comboBoxSharpness.Text);
+
+            Weapon thisWeapon = new Weapon(SetClassModifier(comboBoxClass.Text), float.Parse(textBoxRawDamage.Text),
+                                           float.Parse(textBoxEDamage.Text), float.Parse(textBoxAffinity.Text),
+                                           sharpnessValues[0], sharpnessValues[1]);
         }
     }
 }
--- MonsterHunterWorldWeaponCalculator/MonsterHunterWorldWeaponCalculator/Weapon.cs
+++ MonsterHunterWorldWeaponCalculator/MonsterHunterWorldWeaponCalculator/Weapon.cs
@@ -8,21 +8,32 @@ namespace MonsterHunterWorldWeaponCalculator
 {
     class Weapon
     {
-        public float Class { get; set; }
+        public float WeaponClass { get; set; }
         public float DefaultRawDamage { get; set; }
         public float DefaultElementalDamage { get; set; }
         public float DefaultAffinity { get; set; }
         public float DefaultRawSharpness { get; set; }      //.5=red; .75=orange; 1=yellow; 1.05=green; 1.2=blue; 1.32=white
-        public int DefualtElementalSharpness { get; set; } //.25=red; .5=orange; .75=yellow; 1=green; 1.0625=blue; 1.125=white
+        public float DefualtElementalSharpness { get; set; } //.25=red; .5=orange; .75=yellow; 1=green; 1.0625=blue; 1.125=white
 
-        public Weapon()
+        public Weapon(float weaponClass,
+                      float rawDamage,
+                      float eDamage,
+                      float affinity,
+                      float sharpness,
+                      float eSharpness)
         {
-
+            WeaponClass = weaponClass;
+            DefaultRawDamage = rawDamage;
+            DefaultElementalDamage = eDamage;
+            DefaultAffinity = affinity;
+            DefaultRawSharpness = sharpness;
+            DefualtElementalSharpness = eSharpness;
         }
 
         public float CalculateDamage(float attackBuff, float affinityBuff)
         {
             float calculatedDamage = (DefaultRawDamage * DefaultRawSharpness /* * motion * monsterArmor * quest/rage? */ );
+            return calculatedDamage;
         }
     }
 }
